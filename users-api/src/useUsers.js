4a768758c555cbc8204d69ae970bc597fb2f32d2
Joel Bartlett <joel@murbar.com>
2019-03-18 17:50:02 -0400

    Implement get all and add

--- users-api/src/useUsers.js
+++ users-api/src/useUsers.js
@@ -1,29 +1,62 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const endpoint = 'http://localhost:4000/api/users';
+
+const userUrl = id => `${endpoint}/${id}`;
+
 const useUsers = () => {
-  const [state, setState] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {}, []);
+  const requestWrapper = cb => async (...args) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      await cb(...args);
+      setIsLoading(false);
+    } catch (error) {
+      console.dir(error);
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
 
-  const getAll = () => {};
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
-  const get = userId => {};
+  const getAllUsers = requestWrapper(async () => {
+    const { data } = await axios.get(endpoint);
+    setUsers(data);
+  });
 
-  const add = user => {};
+  // const get = requestWrapper(async userId => {
+  //   const { data } = await axios.get(userUrl(userId));
+  //   console.log(data);
+  //   return data;
+  // });
 
-  const update = user => {};
+  const addUser = requestWrapper(async user => {
+    const { data } = axios.post(endpoint, user);
+    user.id = data.id;
+    setUsers(prev => [...prev, user]);
+  });
 
-  const remove = userId => {};
+  const updateUser = requestWrapper(async user => {});
+
+  const removeUser = requestWrapper(async userId => {});
 
   return {
-    state,
-    setState,
-    getAll,
-    get,
-    add,
-    update,
-    remove
+    users,
+    isLoading,
+    error,
+    getAllUsers,
+    // get,
+    addUser,
+    updateUser,
+    removeUser
   };
 };
 
