7cefdb5ee5bf4386f9efcdf48bbd3ed499eda6c2
Joel Bartlett <joel@murbar.com>
2019-03-18 17:50:34 -0400

    Render list of users

--- users-api/src/App.js
+++ users-api/src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import useUsers from './useUsers';
+import UserList from './UserList';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Users API demo</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const { users, isLoading, error, addUser, updateUser, removeUser } = useUsers();
+
+  return (
+    <div className="App">
+      <h1>Users API demo</h1>
+      {isLoading && (
+        <div className="loading">
+          <img src="/loading.svg" alt="Loading..." />
+        </div>
+      )}
+      {error && <div className="error">{error}</div>}
+      {users.length ? <UserList users={users} /> : <div>No users</div>}
+    </div>
+  );
+};
 
 export default App;
