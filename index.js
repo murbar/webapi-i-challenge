b9f3fecf23a9c2a42afac33085a4f76610068418
Joel Bartlett <joel@murbar.com>
2019-03-18 15:38:19 -0400

    Add GET all route

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// implement your API here
+const express = require('express');
+const server = express();
+const db = require('./data/db');
+
+const port = 4000;
+
+server.get('/api/users', (req, res) => {
+  db.find
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'The users information could not be retrieved.' });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  res.send('Hello world');
+});
+
+server.post('/api/users', (req, res) => {
+  res.send('Hello world');
+});
+
+server.put('/api/users/:id', (req, res) => {
+  res.send('Hello world');
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  res.send('Hello world');
+});
+
+server.listen(port, () => {
+  console.log(`\nServer running on localhost:${port}`);
+});
