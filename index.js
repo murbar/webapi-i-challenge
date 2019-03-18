268ad5ab9c70223684299661f496f54171cae577
Joel Bartlett <joel@murbar.com>
2019-03-18 16:06:01 -0400

    Add POST user

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const db = require('./data/db');
 
 const port = 4000;
 
+server.use(express.json());
+
 server.get('/api/users', (req, res) => {
   db.find()
     .then(users => {
@@ -29,7 +31,18 @@ server.get('/api/users/:id', (req, res) => {
 });
 
 server.post('/api/users', (req, res) => {
-  res.send('Hello world');
+  const { body: user } = req;
+  if (!user.name || !user.bio) {
+    res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+  } else {
+    db.insert(user)
+      .then(newUser => {
+        res.status(201).json(newUser);
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'There was an error while saving the user to the database' });
+      });
+  }
 });
 
 server.put('/api/users/:id', (req, res) => {
